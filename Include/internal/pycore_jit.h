71773e391b3cde1f43b53ccc1532d197de1aeb60
Savannah Ostrowski <savannahostrowski@gmail.com>
2024-10-10 16:49:03 -0700

    fix indentation

--- Include/internal/pycore_jit.h
+++ Include/internal/pycore_jit.h
@@ -11,13 +11,13 @@ extern "C" {
 
 #ifdef _Py_JIT
 
-    #if defined(__x86_64__)
-        // For x86_64, use preserve_none
-        typedef _Py_CODEUNIT *(*jit_func)(_PyInterpreterFrame *, _PyStackRef *, PyThreadState *) __attribute__((preserve_none));
-    #else
-        // For other platforms, use the default calling convention
-        typedef _Py_CODEUNIT *(*jit_func)(_PyInterpreterFrame *, _PyStackRef *, PyThreadState *);
-    #endif
+#if defined(__x86_64__)
+    // For x86_64, use preserve_none
+    typedef _Py_CODEUNIT *(*jit_func)(_PyInterpreterFrame *, _PyStackRef *, PyThreadState *) __attribute__((preserve_none));
+#else
+    // For other platforms, use the default calling convention
+    typedef _Py_CODEUNIT *(*jit_func)(_PyInterpreterFrame *, _PyStackRef *, PyThreadState *);
+#endif
 
 int _PyJIT_Compile(_PyExecutorObject *executor, const _PyUOpInstruction *trace, size_t length);
 void _PyJIT_Free(_PyExecutorObject *executor);
